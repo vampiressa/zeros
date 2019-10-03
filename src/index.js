3f568d1b693afcde15e222c4d002e698ab338e7f
vampiressa <yuliadervenkova@gmail.com>
2019-10-03 11:39:19 +0300

    zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function zeros(expression) {
-  // your solution
-}
+  const splitted = expression.split('*');
+
+  const result = splitted.map((item) => {
+    let sum = BigInt(1);
+    const number = parseInt(item);
+
+    const isDoubleFactorial = item.includes('!!');
+    const isEven = number % 2 === 0;
+
+    for (let i = (isDoubleFactorial && isEven) ? 2 : 1; i <= number; isDoubleFactorial ? i += 2 : i++) {
+      sum = BigInt(sum) * BigInt(i);
+    }
+    return sum;
+  });
+
+  const endZeroes = /0+$/.exec(result.reduce((a, b) => BigInt(a) * BigInt(b)));
+  return endZeroes ? endZeroes[0].length : 0;
+};
